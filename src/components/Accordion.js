8824351f19c781893b0509f67a92325305f974f5
Jeroen Zwart <jeroen.zwart@live.com>
2021-01-14 13:08:40 +0100

    first commit

---
+++ src/components/Accordion.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  // sets the active index with the useState hook.
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index); //sets the active index to the index that is clicked
+  };
+
+  const renderedItems = items.map((item, index) => {
+
+    const active = index === activeIndex ? "active" : "";
+
+      return <React.Fragment key={item.title}>
+        <div onClick={() => onTitleClick(index)} className={`title ${active}`}>
+          <i className="dropdown icon"></i>
+           {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+  });
+
+  return <div className="ui styled accordion">
+  {renderedItems}
+  </div>;
+};
+
+export default Accordion;
